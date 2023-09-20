395671c6939b8959817e9a988ed95ac0da72e58c
michallves <maicon159951@outlook.com>
2023-09-20 09:03:07 -0300

    adicionado projeto

--- ATT1/superesportes/src/main/java/edu/pw2/superesportes/model/AtletaDados.java
+++
@@ -1,5 +0,0 @@
-package edu.pw2.superesportes.model;
-
-public record AtletaDados(String nome, int idade, 
-                          double peso,double altura) {}
-
--- ATT1/superesportes/src/main/java/edu/pw2/superesportes/model/PartidaDados.java
+++
@@ -1,5 +0,0 @@
-package edu.pw2.superesportes.model;
-
-import java.time.LocalDateTime;
-
-public record PartidaDados(String local, LocalDateTime inicio, LocalDateTime fim) {}
